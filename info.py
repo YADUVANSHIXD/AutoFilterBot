0405c8081fe08a3d45f705b5731153acea8d2e17
Pradeep Yadav <136789010+YADUVANSHIXD@users.noreply.github.com>
2024-08-28 22:54:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -109,7 +109,7 @@ if 'DYNO' in environ:
     APP_NAME = environ.get('APP_NAME')
 else:
     ON_HEROKU = False
-BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'convinced-elaina-movieboot-6853780e.koyeb.app' '0.0.0.0'))
+BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'rigid-ofella-movieboot-cd56b895.koyeb.app' ''))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
 URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
     "https://{}/".format(FQDN, PORT)
